d1c359918f37e18250db6eff56288a437b075662
Loes Alleman <loes.alleman@visma.com>
2023-10-05 13:12:50 +0200

    v1 api endpoints /ratings POST+GET

---
+++ app/Http/Controllers/ApiRatingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rating;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class ApiRatingController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function index(Request $request)
+    {
+        return Rating::where("pdok_id", $request->pdokId)->get();
+    }
+
+    public function store(Rating $rating, Request $request)
+    {
+        $rating->score = $request->score;
+        $rating->pdok_id = $request->pdokId;
+        $rating->pdok_latitude = 0;
+        $rating->pdok_longitude = 0;
+        $rating->save();
+        return $rating;
+    }
+}
