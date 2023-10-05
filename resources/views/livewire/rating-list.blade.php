25310e618ff00285ebe21ea6d0a7c4b0f8ee8727
jmfronik@gmail.com <jordy@Jordys-MacBook-Pro.local>
2023-10-05 13:02:03 +0200

    setup map

--- resources/views/livewire/rating-list.blade.php
+++ resources/views/livewire/rating-list.blade.php
@@ -1,31 +1,48 @@
+<script>
+    async function fetchPosts() {
+      return await (await fetch('/api/ratings')).json();
+    }
 
-  
-  <section class="min-h-screen relative">
-
-    <div x-data="{ result: null }">
-        <button @click="fetchData()">Fetch Data</button>
-        <div x-text="result"></div>
-        <script>
-          function fetchData() {
-            fetch('/api/ratings')
-              .then(response => response.json())
-              .then(data => {
-                // this.result = JSON.stringify(data);
-                console.log(data)
-              })
-              .catch(error => {
-                console.error('Error fetching data:', error);
-              });
-          }
-        </script>
-        </div>
-        <x-mapbox class="h-[70vh]" :markers="[[13.4105300, 52.5243700], [4.895168, 52.370216]]"  :options="['zoom' => 11, 'style' => 'mapbox://styles/mapbox/light-v11', 'center' => [4.895168, 52.370216]]" />   
     
+</script>
 
-        <aside class="fixed bottom-0 w-full">
-            <p>Hello World</p>
+<div
+    x-data="{ posts: [], locationDetails: [] }"
+    x-init="(() => {
+        fetchPosts().then(data => {
+            // Log the data
+            console.log('Raw Data:', data);
 
-            <div
+            // Transform the data as needed
+            const transformedData =  data.data.map((item) => {
+                return {
+                    ...item,
+                    latlng: [item.pdok_latitude, item.pdok_longitude]
+                }
+            })
+
+
+            // Arrays of [item.pdok_latitude, item.pdok_longitude]
+            // Arrays of detail elements like we have now
+
+            {{-- const transformedData = data.map(post => ({ --}}
+                {{-- // Example: Transform the post title to uppercase --}}
+                {{-- title: post.title.toUpperCase(), --}}
+                {{-- // Include other transformations or filtering as needed --}}
+            {{-- })); --}}
+
+            // Assign the transformed data to the 'posts' variable
+        });
+    })()"
+    class="min-h-screen relative"
+>
+    <x-mapbox class="h-[70vh]" :markers="[[13.4105300, 52.5243700], [4.895168, 52.370216]]"  :options="['zoom' => 11, 'style' => 'mapbox://styles/mapbox/light-v11', 'center' => [4.895168, 52.370216]]" />   
+
+
+    <aside class="fixed z-50 bottom-0 w-full rounded-xl border p-8 pb-32 bg-white shadow-md">
+        <p>Hello World</p>
+
+        <div
             x-data="{
                 search: '',
                 items: ['foo', 'bar', 'baz'],
@@ -46,23 +63,4 @@
             </ul>
         </div>
     </aside>
-</section>
-
-{{-- 
-
-
 </div>
-
-<x-filament::button tag="a" href="{{ route('ratings.create') }}">
-    New rating
-</x-filament::button>
-
-<ul class="flex flex-col gap-8">
-    @foreach($ratings as $rating)
-        <li class="p-4 bg-gray-200">
-            <p>PDOK ID: {{ $rating->pdok_id }}</p>
-            <p>Rating: {{ $rating->score }}</p>
-            <p>Comment: {{ $rating->comment }}</p>
-        </li>
-    @endforeach
-</ul> --}}
